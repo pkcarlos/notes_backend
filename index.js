709e40a6572f01423e997b058e060bd7a1e08a95
pkcarlos <patricia.k.carlos@gmail.com>
2024-10-08 14:59:16 -0700

    changed base url

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
+const PORT = config.PORT || 3001
 
-app.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
+app.listen(PORT, () => {
+  logger.info(`Server running on port ${PORT}`)
 })
 
 // imports actual application from app.js file, then starts application

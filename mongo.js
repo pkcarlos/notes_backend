fe93e2361e00981177e9c445e96cf1609a8d2316
pkcarlos <patricia.k.carlos@gmail.com>
2024-10-09 20:01:36 -0700

    remove eslint

--- mongo.js
+++ mongo.js
@@ -12,28 +12,29 @@ const url =
 
 mongoose.set('strictQuery',false)
 
-mongoose.connect(url)
+mongoose.connect(url).then(() => {
+  const noteSchema = new mongoose.Schema({
+    content: String,
+    important: Boolean,
+  })
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
+  const Note = mongoose.model('Note', noteSchema)
+
+  /*
+  const note = new Note({
+    content: 'HTML is x',
+    important: true,
+  })
+
+  note.save().then(result => {
+    console.log('note saved!')
+    mongoose.connection.close()
+  })
+  */
+  Note.find({}).then(result => {
+    result.forEach(note => {
+      console.log(note)
+    })
+    mongoose.connection.close()
+  })
 })
-
-const Note = mongoose.model('Note', noteSchema)
-
-const note = new Note({
-  content: "Close open",
-  important: false,
-})
-
-note.save().then(result => {
-  console.log('note saved!')
-  mongoose.connection.close()
-})
-
-// Note.find({}).then(result => {
-//   result.forEach(note => {
-//     console.log(note)
-//   })
-//   mongoose.connection.close()
-// })

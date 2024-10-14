e8c8b2d3c2eebfea2a09a48bc2aa1d1901ed3df9
pkcarlos <patricia.k.carlos@gmail.com>
2024-10-14 16:03:45 -0700

    added tests

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -1,29 +1,23 @@
 const { test, after, beforeEach } = require('node:test')
-const Note = require('../models/note')
-const mongoose = require('mongoose')
-const supertest = require('supertest')
-const app = require('../app')
 const assert = require('node:assert')
+const supertest = require('supertest')
+const mongoose = require('mongoose')
+
+const helper = require('./test_helper')
+const app = require('../app')
 const api = supertest(app)
 
-const initialNotes = [
-  {
-    content: 'HTML is easy',
-    important: false,
-  },
-  {
-    content: 'Browser can execute only JavaScript',
-    important: true,
-  },
-]
+const Note = require('../models/note')
 
-// ensures the database is the same state before each test is run
-// database is cleared out, two notes are stored
 beforeEach(async () => {
   await Note.deleteMany({})
-  let noteObject = new Note(initialNotes[0])
+
+
+  let noteObject = new Note(helper.initialNotes[0])
   await noteObject.save()
-  noteObject = new Note(initialNotes[1])
+
+
+  noteObject = new Note(helper.initialNotes[1])
   await noteObject.save()
 })
 
@@ -34,20 +28,22 @@ test('notes are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 })
 
-test('there are two notes', async () => {
+test('all notes are returned', async () => {
   const response = await api.get('/api/notes')
 
-  assert.strictEqual(response.body.length, initialNotes.length)
+
+   assert.strictEqual(response.body.length, helper.initialNotes.length)
 })
 
-test('the first note is about HTTP methods', async () => {
+test('a specific note is within the returned notes', async () => {
   const response = await api.get('/api/notes')
 
-  const contents = response.body.map(e => e.content)
-  assert(contents.includes('HTML is easy'))
+  const contents = response.body.map(r => r.content)
+
+  assert(contents.includes('Browser can execute only JavaScript'))
 })
 
-test('a valid note can be added', async () => {
+test('a valid note can be added ', async () => {
   const newNote = {
     content: 'async/await simplifies making async calls',
     important: true,
@@ -58,17 +54,32 @@ test('a valid note can be added', async () => {
     .send(newNote)
     .expect(201)
     .expect('Content-Type', /application\/json/)
-  
-  const response = await api.get('/api/notes')
 
-  const contents = response.body.map(r => r.content)
 
-  assert.strictEqual(response.body.length, initialNotes.length + 1)
+  const notesAtEnd = await helper.notesInDb()
+  assert.strictEqual(notesAtEnd.length, helper.initialNotes.length + 1)
 
+
+  const contents = notesAtEnd.map(n => n.content)
   assert(contents.includes('async/await simplifies making async calls'))
 })
 
-// close database connection
+test('note without content is not added', async () => {
+  const newNote = {
+    important: true
+  }
+
+  await api
+    .post('/api/notes')
+    .send(newNote)
+    .expect(400)
+
+  const notesAtEnd = await helper.notesInDb()
+
+
+  assert.strictEqual(notesAtEnd.length, helper.initialNotes.length)
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
